e30e6d9c575caa81d4b8c3cb60d93a69cc40f21d
izzy2541 <isabel.donnelly@hotmail.com>
2025-08-27 09:17:52 +1000

    Setting up authentication using sanctum

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
@@ -11,7 +12,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required'|'email',
+            'email' => 'required|email',
             'password' => 'required'
         ]);
 
@@ -19,8 +20,28 @@ class AuthController extends Controller
 
         if(!$user){
             throw ValidationException::withMessages([
-                'email' => ['The provided crednetials are incorrect.']
+                'email' => ['The provided credentials are incorrect.']
             ]);
         }
+
+        // here we are comparing the request password and the user password.
+        //if they dont match, this will return false.
+        if(!Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.']
+            ]);
+        }
+
+        //create token is being called from inside HasApiTokens.php
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+
     }
 }
