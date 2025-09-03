5e369f489278e5aefc673f47d1e0655e86f54058
izzy2541 <isabel.donnelly@hotmail.com>
2025-09-03 16:03:04 +1000

    reminder about events

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
+use App\Notifications\EventReminderNotification;
+
 
 class SendEventReminders extends Command
 {
@@ -26,19 +28,25 @@ class SendEventReminders extends Command
      */
     public function handle()
     {
-        //use with to fetch the events together with attendees
         $events = \App\Models\Event::with('attendees.user')
-        ->whereBetween('start_time', [now(), now()->addDay()])
-        ->get();
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
 
         $eventCount = $events->count();
         $eventLabel = Str::plural('event', $eventCount);
+
         $this->info("Found {$eventCount} {$eventLabel}.");
 
         $events->each(
-            fn ($event) => $event->attendees->each(
-                fn($attendee) => $this->info("Notifying the user {$attendee->user->id}"))
-            );
+            fn($event) => $event->attendees->each(
+                fn($attendee) => $attendee->user->notify(
+                    new EventReminderNotification(
+                        $event
+                    )
+                )
+            )
+        );
+
         $this->info('Reminder notifications sent successfully!');
     }
 }
